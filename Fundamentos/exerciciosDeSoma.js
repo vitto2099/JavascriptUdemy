6131b7af4ade3b8011fcd7b2bc02691c38a5fb87
vitto2099 <vck987123@gmail.com>
2025-01-02 14:38:35 -0300

    Adiciona exercícios de soma e lista de compras em JavaScript

---
+++ Fundamentos/exerciciosDeSoma.js
@@ -0,0 +1,3 @@
+function soma(a, b) { console.log(a + b); }
+
+soma(100, 8); // Chama a função e exibe 108 
---
+++ Fundamentos/listadecompras.js
@@ -0,0 +1,2 @@
+const listaDeCompras = ["arroz", "feijao", "batata", "macarrao", "cenoura", "cebola", "alho", "sal", "acucar", "cafe"]
+console.log(listaDeCompras[6]); // Acessa o primeiro item: "arroz"
